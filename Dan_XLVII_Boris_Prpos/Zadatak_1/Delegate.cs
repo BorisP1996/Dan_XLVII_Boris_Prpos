b7a67b5f03f1d9c37c1b8e8b5af6c43e81d581f0
BorisP1996 <prposboris13@hotmail.com>
2020-07-16 14:45:17 +0200

    stopwatch added

---
+++ Dan_XLVII_Boris_Prpos/Zadatak_1/Delegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Delegate
+    {
+        public delegate void Notification();
+
+        public event Notification OnNotification;
+
+        public void Ready(int carNum,List<Thread> list)
+        {
+            OnNotification += () =>
+            {
+                Console.WriteLine("Total number of cars is:{0}\n", carNum);
+                Console.WriteLine("Directions of cars:\n");
+                foreach (Thread item in list)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+            };
+            OnNotification.Invoke();
+        }
+    }
+}
--- Dan_XLVII_Boris_Prpos/Zadatak_1/Program.cs
+++ Dan_XLVII_Boris_Prpos/Zadatak_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,13 +11,18 @@ namespace Zadatak_1
     class Program
     {
         static List<Thread> threadList = new List<Thread>();
+        static CountdownEvent countdown = new CountdownEvent(1);
+        static readonly object locker = new object();
 
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Delegate d = new Delegate();
             Random rnd = new Random();
             int carNum = rnd.Next(1, 16);
             string[] directions = new string[] { "North", "South" };
-            Console.WriteLine("Number of cars is :{0}\n\n\n",carNum);
+            //Console.WriteLine("Number of cars is :{0}\n\n\n",carNum);
             for (int i = 0; i < carNum; i++)
             {
                 int directionRan = rnd.Next(0, 2);
@@ -27,24 +33,43 @@ namespace Zadatak_1
                 };
                 threadList.Add(t);
             }
-
+            d.Ready(carNum, threadList);
+            Console.WriteLine();
             for (int i = 0; i < threadList.Count; i++)
             {
+                int counter = 0;
                 threadList[i].Start();
                 Thread.Sleep(100);
-                if ((i < threadList.Count - 1) && /*(threadList[i + 1].Name != threadList[i].Name)*/ CompareNames(threadList[i].Name,threadList[i+1].Name)==false)
+                if ((i < threadList.Count - 1) && /*(threadList[i + 1].Name != threadList[i].Name)*/ CompareNames(threadList[i].Name, threadList[i + 1].Name) == false)
                 {
-                    Console.WriteLine("Car {0} is waiting for bridge to be open", threadList[i + 1].Name);
+                    Console.WriteLine("\tCar {0} is waiting for bridge to be open", threadList[i + 1].Name);
                     threadList[i].Join();
+
                 }
+                //lock (locker)
+                //{
+                //    counter++;
+                if (threadList[i] == threadList[threadList.Count - 1])
+                {
+                    threadList[i].Join();
+                    countdown.Signal();
+                }
+                //}                 
             }
+            if (countdown.IsSet)
+            {
+                sw.Stop();
+                Console.WriteLine("\n Total time spent in application : {0}h {1}m {2}s {3}ms", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds);
+            }
+            
+            
             Console.ReadLine();
         }
         static void Go(Thread t)
         {
             Console.WriteLine("{0} going over the bridge.",t.Name);
             Thread.Sleep(2000);
-            Console.WriteLine("\t{0} has crossed the bridge.",t.Name);
+            Console.WriteLine("\t\t{0} has crossed the bridge.",t.Name);
         }
         static bool CompareNames(string s1, string s2)
         {
@@ -71,5 +96,6 @@ namespace Zadatak_1
                 }
             }
         }
+
     }
 }
